bf2f38c662b746850c8fbcd1e12581ac05530eba
gabri <gabrielrl2004@gmail.com>
2024-02-18 17:38:59 +0100

    Initial commit

---
+++ src/main/java/com/example/turismApi/TurismApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.turismApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurismApiApplication.class, args);
+	}
+
+}
