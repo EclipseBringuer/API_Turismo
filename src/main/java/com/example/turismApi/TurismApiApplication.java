46d6d15c52ab92d43b6c69b451d71717bb52a755
gabri <gabrielrl2004@gmail.com>
2024-02-20 16:32:58 +0100

    Uso de la documentacion de la api en su inicio

--- src/main/java/com/example/turismApi/TurismApiApplication.java
+++ src/main/java/com/example/turismApi/TurismApiApplication.java
@@ -1,13 +1,15 @@
 package com.example.turismApi;
 
+import com.example.turismApi.utilities.Utility;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TurismApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TurismApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TurismApiApplication.class, args);
+        Utility.openDocumentation();
+    }
 
 }
