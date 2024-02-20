3213fabb0f103f9f19e93b71332627eed9f83895
gabri <gabrielrl2004@gmail.com>
2024-02-20 16:51:22 +0100

    Renombre del DTO del usuario

---
+++ src/main/java/com/example/turismApi/model/dto/InfoUsuarioDTO.java
@@ -0,0 +1,4 @@
+package com.example.turismApi.model.dto;
+
+public record InfoUsuarioDTO(String name, String gmail) {
+}
--- src/main/java/com/example/turismApi/model/dto/UsuarioDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.turismApi.model.dto;
-
-public record UsuarioDTO(String name, String gmail) {
-}
