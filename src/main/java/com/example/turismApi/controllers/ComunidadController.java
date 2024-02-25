ea40b1c259c9b70399db090d2af8c9eb332e0b51
gabri <gabrielrl2004@gmail.com>
2024-02-25 21:19:48 +0100

    Metodos finales en comunidad añadidos

--- src/main/java/com/example/turismApi/controllers/ComunidadController.java
+++ src/main/java/com/example/turismApi/controllers/ComunidadController.java
@@ -1,11 +1,13 @@
 package com.example.turismApi.controllers;
 
-import com.example.turismApi.model.entity.Comunidad;
+import com.example.turismApi.model.dto.comunidad.InfoComunidadDTO;
 import com.example.turismApi.services.ComunidadService;
+import com.example.turismApi.services.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -13,9 +15,29 @@ import java.util.List;
 public class ComunidadController {
     @Autowired
     private ComunidadService comunidadService;
+    @Autowired
+    private SecurityService securityService;
 
     @GetMapping("")
-    public List<Comunidad> getAllComunidades(){
-        return comunidadService.getAllComunidades();
+    public ResponseEntity<List<InfoComunidadDTO>> getAllComunidades(@RequestParam("token") String token) {
+        if (securityService.validateToken(token)) {
+            return new ResponseEntity<>(comunidadService.getAllComunidades(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<InfoComunidadDTO> getById(@PathVariable Integer id, @RequestParam("token") String token) {
+        if (securityService.validateToken(token)) {
+            var output = comunidadService.getById(id);
+            if (output != null) {
+                return new ResponseEntity<>(output, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
     }
 }
--- src/main/java/com/example/turismApi/services/ComunidadService.java
+++ src/main/java/com/example/turismApi/services/ComunidadService.java
@@ -1,10 +1,16 @@
 package com.example.turismApi.services;
 
+import com.example.turismApi.model.dto.comunidad.InfoComunidadDTO;
+import com.example.turismApi.model.dto.comunidad.InfoProvinciaEnComunidadDTO;
 import com.example.turismApi.model.entity.Comunidad;
+import com.example.turismApi.model.entity.Provincia;
 import com.example.turismApi.repositories.ComunidadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ComunidadService {
@@ -15,7 +21,30 @@ public class ComunidadService {
         this.comunidadRepository = comunidadRepository;
     }
 
-    public List<Comunidad> getAllComunidades(){
-        return comunidadRepository.findAll();
+    public List<InfoComunidadDTO> getAllComunidades() {
+        var output = comunidadRepository.findAll();
+        return output.stream()
+                .map(c -> new InfoComunidadDTO(c.getId(), c.getName(), convertProvinciaToInfoDTO(c.getProvincias())))
+                .collect(Collectors.toList());
+    }
+
+    public InfoComunidadDTO getById(Integer id) {
+        InfoComunidadDTO output = null;
+        if (comunidadRepository.existsById(id)) {
+            output = convertComunidadToInfoDTO(comunidadRepository.findById(id).get());
+        }
+        return output;
+    }
+
+    private InfoComunidadDTO convertComunidadToInfoDTO(Comunidad comunidad) {
+        return new InfoComunidadDTO(comunidad.getId(), comunidad.getName(), convertProvinciaToInfoDTO(comunidad.getProvincias()));
+    }
+
+    private List<InfoProvinciaEnComunidadDTO> convertProvinciaToInfoDTO(List<Provincia> list) {
+        List<InfoProvinciaEnComunidadDTO> output = new ArrayList<>();
+        for (Provincia p : list) {
+            output.add(new InfoProvinciaEnComunidadDTO(p.getId(), p.getName(), p.getPuntosInteres().size()));
+        }
+        return output;
     }
 }
