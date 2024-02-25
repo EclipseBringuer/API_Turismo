249f3a609ab928126b04a3eea50fd4291638bbae
gabri <gabrielrl2004@gmail.com>
2024-02-25 19:09:33 +0100

    Comentario Repository con metodos nuevos

--- src/main/java/com/example/turismApi/repositories/ComentarioRepository.java
+++ src/main/java/com/example/turismApi/repositories/ComentarioRepository.java
@@ -4,6 +4,10 @@ import com.example.turismApi.model.entity.Comentario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ComentarioRepository extends JpaRepository<Comentario, Integer> {
+    List<Comentario> getAllByUsuario_Id(Integer id);
+    List<Comentario> getAllByPoi_Id(Integer id);
 }
