b99584c21aec67b3fc214ec444f34a60c384ac2e
gabri <gabrielrl2004@gmail.com>
2024-02-25 17:16:08 +0100

    Servicio de seguridad con generación de token y validación del mismo

--- src/main/java/com/example/turismApi/services/SecurityService.java
+++ src/main/java/com/example/turismApi/services/SecurityService.java
@@ -1,15 +1,19 @@
 package com.example.turismApi.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityService {
+    @Autowired
     private UsuarioService usuarioService;
+
     public boolean validateToken(String token) {
-        return true;
+        return usuarioService.getByToken(token) != null;
     }
 
     public String generateToken() {
-        return "";
+        int tokenNumber = usuarioService.getMaxId() + 1;
+        return "t0ken" + tokenNumber;
     }
 }
