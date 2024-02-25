1b1b1234d68ac03c9a9b82ba5ad5d1cca2299257
gabri <gabrielrl2004@gmail.com>
2024-02-25 17:18:45 +0100

    Metodo de obtener usuario mediante token añadido. Metodo para sacar el maximo id añadido

--- src/main/java/com/example/turismApi/repositories/UsuarioRepository.java
+++ src/main/java/com/example/turismApi/repositories/UsuarioRepository.java
@@ -2,8 +2,12 @@ package com.example.turismApi.repositories;
 
 import com.example.turismApi.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Usuario getByToken(String token);
+    @Query("SELECT u.id FROM Usuario u ORDER BY u.id DESC LIMIT 1")
+    Integer getMaxId();
 }
