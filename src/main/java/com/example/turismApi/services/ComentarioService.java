96199f950db631d6665721d6dfe1daf0606c2cd9
gabri <gabrielrl2004@gmail.com>
2024-02-25 19:10:03 +0100

    Metodos para obtener, crear y borrar comentarios implementados en la capa de servicio

--- src/main/java/com/example/turismApi/services/ComentarioService.java
+++ src/main/java/com/example/turismApi/services/ComentarioService.java
@@ -1,22 +1,82 @@
 package com.example.turismApi.services;
 
+import com.example.turismApi.model.dto.comentario.InfoComentarioDTO;
+import com.example.turismApi.model.dto.comentario.NewComentarioDTO;
 import com.example.turismApi.model.entity.Comentario;
 import com.example.turismApi.repositories.ComentarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class ComentarioService {
     private final ComentarioRepository comentarioRepository;
+    @Autowired
+    private UsuarioService usuarioService;
+    @Autowired
+    private PuntoInteresService puntoInteresService;
 
     @Autowired
     public ComentarioService(ComentarioRepository comentarioRepository) {
         this.comentarioRepository = comentarioRepository;
     }
 
-    public List<Comentario> getAll() {
-        return comentarioRepository.findAll();
+    public List<InfoComentarioDTO> getAll() {
+        var output = comentarioRepository.findAll();
+        return output.stream()
+                .map(c -> new InfoComentarioDTO(c.getUsuario().getName(), c.getPoi().getName(), c.getContenido()))
+                .collect(Collectors.toList());
+    }
+
+    public List<InfoComentarioDTO> getAllByUser(Integer id) {
+        var output = comentarioRepository.getAllByUsuario_Id(id);
+        return output.stream()
+                .map(c -> new InfoComentarioDTO(c.getUsuario().getName(), c.getPoi().getName(), c.getContenido()))
+                .collect(Collectors.toList());
+    }
+
+    public List<InfoComentarioDTO> getAllByPoi(Integer id) {
+        var output = comentarioRepository.getAllByPoi_Id(id);
+        return output.stream()
+                .map(c -> new InfoComentarioDTO(c.getUsuario().getName(), c.getPoi().getName(), c.getContenido()))
+                .collect(Collectors.toList());
+    }
+
+    public InfoComentarioDTO createNewComentario(String token, NewComentarioDTO comentarioDTO) {
+        InfoComentarioDTO output = null;
+        var user = usuarioService.getByToken(token);
+        var poi = puntoInteresService.getById(comentarioDTO.id_poi());
+        if (poi != null) {
+            var comentario = new Comentario();
+            comentario.setContenido(comentarioDTO.content());
+            comentario.setPoi(poi);
+            comentario.setUsuario(user);
+            output = convertComentarioToInfoDTO(comentarioRepository.save(comentario));
+        }
+        return output;
+    }
+
+    public String deleteComentario(Integer id, String token) {
+        String output = null;
+        var user = usuarioService.getByToken(token);
+        boolean found = false;
+        for (Comentario c : user.getComentarios()) {
+            if (Objects.equals(c.getId(), id)) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            comentarioRepository.deleteById(id);
+            output = "Mensaje borrado correctamente";
+        }
+        return output;
+    }
+
+    private InfoComentarioDTO convertComentarioToInfoDTO(Comentario c) {
+        return new InfoComentarioDTO(c.getUsuario().getName(), c.getPoi().getName(), c.getContenido());
     }
 }
