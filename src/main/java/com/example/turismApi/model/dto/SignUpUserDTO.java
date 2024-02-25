ed72ffc23d67732aa83f8a6ef82c799419528da1
gabri <gabrielrl2004@gmail.com>
2024-02-25 17:16:34 +0100

    DTO para registrar un usuario

---
+++ src/main/java/com/example/turismApi/model/dto/SignUpUserDTO.java
@@ -0,0 +1,12 @@
+package com.example.turismApi.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record SignUpUserDTO(
+        @NotBlank(message = "Introduce un nombre válido")
+        String name,
+        @NotBlank(message = "Introduce un correo válido")
+        String gmail,
+        @NotBlank(message = "La cuenta debe tener una contraseña")
+        String pass) {
+}
