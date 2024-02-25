1901f242e26d0bfbe330f8c8d18c9cf0d03e0a00
gabri <gabrielrl2004@gmail.com>
2024-02-25 17:17:52 +0100

    Metodo de obtener todos los usuarios securizado. Metodo para registrar un usuario añadido

--- src/main/java/com/example/turismApi/controllers/UsuarioController.java
+++ src/main/java/com/example/turismApi/controllers/UsuarioController.java
@@ -1,13 +1,16 @@
 package com.example.turismApi.controllers;
 
 import com.example.turismApi.model.dto.InfoUsuarioDTO;
+import com.example.turismApi.model.dto.SignUpUserDTO;
+import com.example.turismApi.model.entity.Usuario;
+import com.example.turismApi.services.SecurityService;
 import com.example.turismApi.services.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -15,10 +18,21 @@ import java.util.List;
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
+    @Autowired
+    private SecurityService securityService;
 
     @GetMapping("/getAll")
-    public ResponseEntity<List<InfoUsuarioDTO>> getALl() {
-        var output = usuarioService.getAllUsuarioDTO();
-        return new ResponseEntity<>(output, HttpStatus.OK);
+    public ResponseEntity<List<InfoUsuarioDTO>> getALl(@RequestParam("token") String token) {
+        if (securityService.validateToken(token)) {
+            var output = usuarioService.getAllUsuarioDTO();
+            return new ResponseEntity<>(output, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @PostMapping("/signUp")
+    public ResponseEntity<Usuario> signUp(@Valid @RequestBody SignUpUserDTO u){
+        return new ResponseEntity<>(usuarioService.signUp(u,securityService.generateToken()),HttpStatus.OK);
     }
 }
